5e0f9269b7d15c77347a7ba088bd74467e899d39
tsipponen <tsjsip@utu.fi>
2016-03-16 14:54:57 +0200

    Create Action.jsx

---
+++ Action.jsx
@@ -0,0 +1,20 @@
+Action = React.createClass({
+	propTypes: {
+		action: React.PropTypes.object.isRequired
+	},
+	componentWillMount(){
+		console.log(this.props);
+	},
+
+
+
+	
+	render(){
+		return(
+			<span >
+
+			</span>
+			
+		);
+	}
+});
