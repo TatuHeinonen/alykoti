3c19adce690e30c0a68f3f18f00324060567beb2
tsipponen <tsjsip@utu.fi>
2016-03-25 13:41:04 +0200

    Update Window.jsx

--- Window.jsx
+++ Window.jsx
@@ -8,6 +8,11 @@ Window = React.createClass({
 		}
 	},
 
+componentWillMount(){
+	console.log(this.props);
+},
+
+
 renderActions(){
 	// Get actions from this.data.actions
 	return this.data.actions.map((action) => {
@@ -16,8 +21,22 @@ renderActions(){
 },
 
 
-deletoi(){
-	Actions.remove(this.props.action._id);
+handleClick1(event){
+	this.setState({clicked: !this.state.clicked});
+},
+
+functionality1(event1){
+	window.open('http://www.tunturisusi.com/pikkumarmosetti/');
+},
+
+getInitialState(){
+	return {clicked:false}
+},
+
+wannaConfirm(event){
+	if(confirm("Haluatko varmasti valita tämän?")) 
+		alert('Jepjep ')
+		else alert('Nössö')
 },
 
 render(){
@@ -25,28 +44,43 @@ render(){
 		<div className="container">
 			<header>
 				<h1>Valitse Älykoti</h1>
-
-				<form className="login1" onClick={this.deletoi}>
+				<span className="login1">
 					<input
-						className="submitLogin1"
-						type="button" 
+						className="submitLogin1" 					
 						value="Älytalo"
-						readOnly={true}/>							
-			
+						type="button"
+						ref="button1"
+						readOnly={true}
+						/>
 					<input
 					    className="submitLogin2"
 						value="Älymökki"
 						type="button"
+						ref="button2"
 						readOnly={true}/>
 						
-					<input
+					<button
+						onClick={this.wannaConfirm}
 						className="submitLogin3"
 						type="button" 
-						value="Älysauna"
-						readOnly={true}/>	
-				</form>
+						ref="button3"
+					>Älysauna
+					</button>	
+					<button 
+						onClick={this.functionality1}
+						className="buttonTest"
+						type="button" 
+						ref="button4"
+						>Vituttaako koodaus? Tästä söpö marmosetti.
+						
+					</button>
+					
+				</span>
 			</header>
-				{this.renderActions()}
+				
+
+
+
 		</div>
 	);
 }
