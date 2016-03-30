ae25c2921be7aaa21aaf1db4069c280c4b9abdc9
tsipponen <tsjsip@utu.fi>
2016-03-30 12:33:15 +0300

    Update Komponent.jsx

--- Komponent.jsx
+++ Komponent.jsx
@@ -1,8 +1,5 @@
+
 Komponent = React.createClass({
-	
-handleClick(event){
-	
-},
 
 getInitialState(){
 	return{
@@ -10,69 +7,120 @@ getInitialState(){
 }
 },
 
-stateChanger(number){
-	this.setState({innerComponent: number})
+stateChanger(number, nimi, admin){
+	if(this.state > number){
+		if(confirm('Are you sure you want to move to ' + nimi))
+		this.setState({innerComponent: number})
+		else{
+			return false;
+		}
+	}
+	else{
+		if(confirm('Are you sure you want to move to ' + nimi)){
+			if(admin === true){
+				if(prompt('Please enter the password.') === "admin"){
+					this.setState({innerComponent: number})
+				}
+				else{
+					alert('The password you gave is wrong, n00b.')
+				}
+			}
+			else{
+			this.setState({innerComponent: number})
+		}
+		}else{
+			return false;
+		}
+	}
 },
 
 componentHandler(){
 	switch(this.state.innerComponent){
 		case 1:
-		return <AlaKomponent changeState={this.stateChanger}/>
+		return <RoleView changeState={this.stateChanger}/>
+
+		case 2:
+		return <AdminView changeState={this.stateChanger}/>
+
+		case 3:
+		return <ChildView changeState={this.stateChanger}/>
 
 		default:
-		return <PaaNakyma changeState={this.stateChanger}/>
+		return <MainView changeState={this.stateChanger}/>
 	}
 
 },
+
+
 
 render(){
 	return(
 		<div className="container">
 			{this.componentHandler()}
+			
 		</div>
 	);
 }
 });
 
-PaaNakyma = React.createClass({
-	clickHandler(){
-		this.props.changeState(1);
+MainView = React.createClass({
+	clickHandler(number,nimi,admin){
+		this.props.changeState(number,nimi,admin);
 	},
 	render(){
+		const home = "the home-screen?";
+		const isAdministrator = true;
 		return(
 			<div className="container">
 			<header>
-				<h1>Valitse Älykoti</h1>
-					<span className="login1">		
-					<button 
-						onClick={()=>this.clickHandler()}
-						className = "submitLogin1"
-						>
-						Lorenz0n opetus koulu
+				<h1>Choose your smarthouse</h1>
+					<div className="login1">		
+					<button className = "button-home"
+						onClick={()=>this.clickHandler(1, home, !isAdministrator)}
+						>Home
 					</button>
-					
-						
-				</span>
+					<button className ="button-cottage"
+					>Cottage
+					</button>
+				</div>
 			</header>
 		</div>
 			)
 	}
 });
 
-AlaKomponent = React.createClass({
-	clickHandler(){
-		this.props.changeState(0);
+RoleView = React.createClass({
+	clickHandler(number, nimi, admin){
+		this.props.changeState(number,nimi, admin);
 	},
 	render(){
+		const alykoti = "the smarthouse view?";
+		const aikuinen = "the adult view?";
+		const lapsi = "the child view?"
+		const admin = "the administrator view?"
+		const isAdministrator = true;
 		return(
+			
 			<div className ="newContainer">
 				<header>
-					<h1>Valitse rooli</h1>
-						<span className="Mikkopaskaa">
-       			 			<button 
-       			 				onClick={()=> this.clickHandler()}
-       			 				className="submitLogin2"
-       			 			>Lorenz0n oikein kiriotus kolu
+					<h1>Choose your role<button 
+										className="button-takaisin"
+										onClick={()=> this.clickHandler(0, alykoti, !isAdministrator)}
+										>Back</button>
+										</h1>
+						<span className="testing">
+       			 			<button className="button-adult"
+       			 				onClick={()=> this.clickHandler(1, aikuinen, !isAdministrator)}
+       			 				
+       			 			>Adult
+							</button>
+							<button className="button-child"
+								onClick={()=> this.clickHandler(3, lapsi, !isAdministrator)}
+							>Child 
+							</button>
+							<button className="button-admin"
+								onClick={()=> this.clickHandler(2, admin, isAdministrator)}
+							>Administrator
 							</button>
 						</span>
        			</header>
@@ -81,4 +129,68 @@ AlaKomponent = React.createClass({
 			)
 
 	}
-})
+});
+
+AdminView = React.createClass({
+	clickHandler(number, nimi, admin){
+		this.props.changeState(number,nimi,admin);
+	},
+
+	render(){
+		const rooli = "the role view?";
+		const isAdministrator = true;
+		return(
+			<div className="mrAdmin">
+				<header>
+					<h1>Good day, sir! <button 
+										className="button-takaisin"
+										onClick={()=> this.clickHandler(1, rooli, !isAdministrator)}
+										>Back</button>
+					</h1>
+				</header>
+			</div>
+			)
+	}
+
+});
+
+ChildView = React.createClass({
+	clickHandler(number,nimi,admin){
+		this.props.changeState(number,nimi,admin);
+	},
+
+	getInitialState(){
+		return{lightsOn: false}
+	},
+
+	lightsHandler(event){
+		this.setState({lightsOn: !this.state.lightsOn})
+	},
+	render(){
+		const rooli = "the role view?"
+		const isAdministrator = true;
+		let lightsOn = this.state.lightsOn ? 'are on.' : 'are off.';
+		return(
+			<div className="childView">
+				<header>
+					<h1>Hiya, kid!<button 
+										className="button-takaisin"
+										onClick={()=> this.clickHandler(1, rooli, !isAdministrator)}
+										>Back</button>
+					</h1>
+					<span>
+					<button className="button-lightsOn"
+							onClick={()=> this.lightsHandler(lightsOn)}
+
+					>Lights {lightsOn}
+					</button>
+					</span>
+
+				</header>
+
+				</div>
+
+			)
+	}
+
+});
