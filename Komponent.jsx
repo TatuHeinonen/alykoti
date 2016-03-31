a7db50eb5944add5915a26eafc75181066802a9f
tsipponen <tsjsip@utu.fi>
2016-03-31 18:09:36 +0300

    Update Komponent.jsx

--- Komponent.jsx
+++ Komponent.jsx
@@ -1,4 +1,3 @@
-
 Komponent = React.createClass({
 
 getInitialState(){
@@ -45,13 +44,14 @@ componentHandler(){
 		case 3:
 		return <ChildView changeState={this.stateChanger}/>
 
+		case 4:
+		return <AdultView changeState={this.stateChanger}/>
+
 		default:
 		return <MainView changeState={this.stateChanger}/>
 	}
 
 },
-
-
 
 render(){
 	return(
@@ -110,7 +110,7 @@ RoleView = React.createClass({
 										</h1>
 						<span className="testing">
        			 			<button className="button-adult"
-       			 				onClick={()=> this.clickHandler(1, aikuinen, !isAdministrator)}
+       			 				onClick={()=> this.clickHandler(4, aikuinen, !isAdministrator)}
        			 				
        			 			>Adult
 							</button>
@@ -181,15 +181,130 @@ ChildView = React.createClass({
 					<span>
 					<button className="button-lightsOn"
 							onClick={()=> this.lightsHandler(lightsOn)}
-
 					>Lights {lightsOn}
 					</button>
 					</span>
-
 				</header>
-
 				</div>
 
+			)
+	}
+
+});
+
+AdultView = React.createClass({
+	clickHandler(number,nimi,admin){
+		this.props.changeState(number,nimi,admin);
+	},
+
+	getInitialState(){
+		return{lightsOn: false}, {valueOven: 0}, {ovenOn: false}, {tvOn: false}, {tvChannel: null}
+	},
+
+	handleOven(event){
+		this.setState({valueOven: event.target.value});
+	},
+
+	lightsHandler(event){
+		this.setState({lightsOn: !this.state.lightsOn})
+	},
+
+	setOven(event){
+		this.setState({ovenOn: !this.state.ovenOn})
+
+	},
+
+	setTV(event){
+		this.setState({tvOn: !this.state.tvOn})
+
+	},
+
+	setChannel(channel){
+			if(channel === 1){
+				this.setState({tv1: !this.state.tv1})
+				this.setState({tv2: false})
+				this.setState({tvChannel: channel})
+			}
+			else{
+				this.setState({tv2: !this.state.tv2})
+				this.setState({tv1: false})
+				this.setState({tvChannel: channel})
+			}
+	},
+
+	render(){
+		const rooli = "the role view?";
+		const isAdministrator = true;
+		let lightsOn = this.state.lightsOn ? 'are on.' : 'are off.';
+		let ovenOn = this.state.ovenOn ? false : true;
+		let ovenText = this.state.ovenOn ? 'is on.' : 'is off.';
+		let ovenInfo = this.state.ovenOn ? 'Please adjust the oven temperature. Now: '+ this.state.valueOven + ' degrees.' : '';
+		let tvText = this.state.tvOn ? 'on. Please select channel.' : 'off.';
+		let tvOn = this.state.tvOn ? true : false;
+		let currentChannel = this.state.tvOn ? 'You are watching channel ' + this.state.tvChannel + '.' : '';
+		let tv1 = this.state.tv1 ? true : false;
+		let tv2 = this.state.tv2 ? true : false;
+		return(
+			<div className="childView">
+				<header>
+					<h1>Hello!<button 
+										className="button-takaisin"
+										onClick={()=> this.clickHandler(1, rooli, !isAdministrator)}
+										>Back</button>
+					</h1>
+					<span>
+					<button className="button-lightsOn"
+							onClick={()=> this.lightsHandler(lightsOn)}
+					>Lights {lightsOn}
+					</button>
+					<button className="button-oven"
+					onClick={()=> this.setOven()}
+					>
+					The oven {ovenText} 
+					 </button>
+					 <div>
+						{ovenInfo} 
+						</div>
+					 <input 
+						className="slideri"
+						id="slider" 
+						hidden={ovenOn}
+						type="range" 
+						min="0" 
+						max="250" 
+						value={this.state.valueOven}
+						onChange={this.handleOven}
+						step ="10"
+						/>
+						
+						<button className="button-tv"
+						onClick={()=> this.setTV()}
+						>The television is {tvText}
+						</button>
+						<div>
+						{currentChannel}
+						</div>
+						<form >
+						<input 
+						type="radio" 
+						value="channel1" 
+						hidden = {!tvOn}
+						checked = {tv1}
+						onClick={()=> this.setChannel(1)}
+						/> 
+						<br> </br>
+						<input
+						type="radio"
+						value="channel2"
+						hidden = {!tvOn}
+						checked = {tv2}
+						onClick= {()=> this.setChannel(2)}
+						/> 
+						</form>
+					</span>
+				</header>
+				
+				</div>
 			)
 	}
 
